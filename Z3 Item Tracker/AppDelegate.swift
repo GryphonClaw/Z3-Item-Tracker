9478d9040124cf8e2aa047400a68461b6432b1ef
TheCire <ss2cire@gmail.com>
2019-03-02 04:47:09 -0800

    Initial Commit

---
+++ Z3 Item Tracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//  AppDelegate.swift
+//  Z3 Item Tracker
+//
+//  Created by GryphonClaw Software on 3/2/19.
+//  Copyright © 2019 GryphonClaw Software. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Z3 Item Tracker/ViewController.swift
@@ -0,0 +1,27 @@
+//  ViewController.swift
+//  Z3 Item Tracker
+//
+//  Created by GryphonClaw Software on 3/2/19.
+//  Copyright © 2019 GryphonClaw Software. All rights reserved.
+//
+
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
